7587714a829a27e94257c2390d7036551ce85081
ABRaihan <abraihan7244@gmail.com>
2021-03-01 22:00:50 +0600

    done my project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
